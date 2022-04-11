cc775a679b482a7622c0dd796283cba19fff9074
Heng WU <wuheng@iscas.ac.cn>
2022-04-11 19:49:56 +0800

    init

---
+++ protocols/http/src/main/java/io/github/kubesys/http/HttpSender.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (2022, ) Institute of Software, Chinese Academy of Sciences
+ */
+package io.github.kubesys.http;
+
+
+/**
+ * This class is used for creating a connection between users' application and Kubernetes server.
+ * It provides an easy-to-use way to Create, Update, Delete, Get, List and Watch all Kubernetes resources.
+ * 
+ * @author wuheng@iscas.ac.cn
+ * @since  2.0.0
+ */
+public class HttpSender {
+
+
+}
